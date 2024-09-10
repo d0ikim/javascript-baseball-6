fdb40398fdf2203533619adfa2cf3fdce963c099
Doi Kim <154426332+d0ikim@users.noreply.github.com>
2024-09-10 16:02:58 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+    // 숫자 랜덤 생성
+    
+    
+    const input = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+    console.log(`입력한 숫자: ${input}`)
+    
+
+
+
+  }
 }
 
+new App().play();
 export default App;
